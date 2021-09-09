1803c5bb20d0fdfb327f1788c5bdbd0e6f36a56a
Trevor Sears <trevor@trevorsears.com>
2021-09-09 16:12:40 -0400

    Added file header copyright notice.

--- ts/semaphore.ts
+++ ts/semaphore.ts
@@ -2,6 +2,22 @@
  * Created by Trevor Sears <trevor@trevorsears.com> (https://trevorsears.com/).
  * 3:09 PM -- September 09, 2021.
  * Project: @jsdsl/semaphore
+ * 
+ * @jsdsl/semaphore - A Promise-based semaphore implementation.
+ * Copyright (C) 2021 Trevor Sears
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 import "promise-any-polyfill";
